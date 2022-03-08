1d7ff3654f7c85516e2cdbc8c3c79dd3a0bb947c
Jeff Potter <jeff.potter6@gmail.com>
2022-03-07 21:17:21 -0700

    Remove some comments

--- src/calc.ts
+++ src/calc.ts
@@ -1,15 +1,6 @@
 
 export const POUND_TO_KILO = 2.20462262
 
-/*
-Brzycki: weight × (36 / (37 - reps)) (another way to write it)
-Epley: weight × (1 + 0.0333 × reps)
-Lander: (100 × weight) / (101.3 - 2.67123 × reps)
-Lombardi: weight × reps ^ 0.1
-Mayhew et al.: (100 × weight) / (52.2 + (41.9 × e-0.055 × reps))
-O’Conner et al.: weight × (1 + 0.025 × reps)
-Wathan: (100 × weight) / (48.8 + (53.8 × e ^ -0.075 × reps))
-*/
 const NONNEGATIVE_ERROR_MSG = "Negative Numbers not allowed"
 
 export const poundsToKilograms = (pounds: number): number => {
